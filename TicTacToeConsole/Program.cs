43ef29ddd672fbb7614c0defdd2d1bcc6e1b7dce
Ingrid de Jong <ingrid.dejong@student.hu.nl>
2016-06-11 14:21:55 +0200

    Added TicTacToeConsole and Engine

---
+++ TicTacToeConsole/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeConsole
+{
+    class Program
+    {
+
+
+        static void Display(TicTacToeEngine t)
+        {
+            Console.WriteLine("Type a number from 1-9, new or quit\n");
+            Console.WriteLine("Status :" + t.Status + "\n");
+            Console.WriteLine(t.Board());
+        }
+        
+        private static Boolean IsInt(string value)
+        {
+            foreach (char c in value)
+            {
+                int i = (int)c;
+                if((i > 57) || (i < 49))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            TicTacToeEngine t = new TicTacToeEngine();
+            
+            while (true)
+            {
+                if (!(t.Status == TicTacToeEngine.GameStatus.Equal || t.Status == TicTacToeEngine.GameStatus.PlayerOWins || t.Status == TicTacToeEngine.GameStatus.PlayerXWins))
+                {
+                    Display(t);
+                    string input = Console.ReadLine();
+
+                    if (input == "new")
+                    {
+                        t.Reset();
+                        Console.Clear();
+                    }
+                    else if (IsInt(input))
+                    {
+                        t.ChooseCell(int.Parse(input));
+                    }
+                    else if (input == "quit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect Input\nType a number from 1-9, new or quit\n");
+                    }
+                }
+                else
+                {
+                    if(t.Status == TicTacToeEngine.GameStatus.Equal)
+                    {
+                        Console.WriteLine("Nobody wins.\nPress enter to play again");
+                    }
+                    else if (t.Status == TicTacToeEngine.GameStatus.PlayerOWins)
+                    {
+                        Console.WriteLine("Player O wins. Congratulations\nPress enter to play again");
+                    }
+                    else if (t.Status == TicTacToeEngine.GameStatus.PlayerXWins)
+                    {
+                        Console.WriteLine("Player X wins. Congratulations\nPress enter to play again");
+                    }
+                    Console.ReadLine();
+                    Console.Clear();
+                    t.Reset();
+                }
+            }
+            
+        }
+    }
+}
---
+++ TicTacToeConsole/TicTacToeEngine.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeConsole
+{
+    class TicTacToeEngine
+    {
+        public GameStatus Status { get; private set; }
+        //Game state
+        public enum GameStatus { PlayerOPlays, PlayerXPlays, Equal, PlayerOWins, PlayerXWins }
+
+        public string[] spelbord = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+        //String representatie van het bord
+        public String Board()
+        {
+            String line = "-------------\n";
+            return line + "| " + spelbord[0] + " | " + spelbord[1] + " | " + spelbord[2] + "|\n"
+                + line + "| " + spelbord[3] + " | " + spelbord[4] + " | " + spelbord[5] + "|\n"
+                + line + "| " + spelbord[6] + " | " + spelbord[7] + " | " + spelbord[8] + "|\n" + line;
+        }
+
+        //Welk hokje wordt gekozen
+        public Boolean ChooseCell(int x)
+        {
+            x = x - 1;
+            Boolean result = false;
+            if (!Chosen(x))
+            {
+                if (Status == GameStatus.PlayerOPlays)
+                {
+                    spelbord[x] = "O";
+                    if (!allChosen(spelbord))
+                    {
+                        if (!checkWin())
+                        {
+                            Status = GameStatus.PlayerXPlays;
+                            result = true;
+                        }
+                    }
+                    else
+                    {
+                        if (!checkWin())
+                        {
+                            Status = GameStatus.Equal;
+                        }
+                    }
+                }
+                else
+                {
+                    spelbord[x] = "X";
+                    if (!allChosen(spelbord))
+                    {
+                        if (!checkWin())
+                        {
+                            Status = GameStatus.PlayerOPlays;
+                            result = true;
+                        }
+                    }
+                    else
+                    {
+                        if (!checkWin())
+                        {
+                            Status = GameStatus.Equal;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (!checkWin())
+                {
+
+                }
+            }
+            return result;
+        }
+
+
+        public Boolean Chosen(int x)
+        {
+            if (spelbord[x] == "X" || spelbord[x] == "O")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean allChosen(string[] array)
+        {
+            Boolean result = true;
+            foreach (string s in spelbord)
+            {
+                if (!(s == "X" || s == "O"))
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public Boolean checkWin()
+        {
+            if (checkWinner(0, 1, 2) ||
+            checkWinner(3, 4, 5) ||
+            checkWinner(6, 7, 8) ||
+            checkWinner(0, 3, 6) ||
+            checkWinner(1, 4, 7) ||
+            checkWinner(2, 5, 8) ||
+            checkWinner(0, 4, 8) ||
+            checkWinner(2, 4, 6))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public Boolean checkWinner(int a, int b, int c)
+        {
+            Boolean result = false;
+            if (spelbord[a] == spelbord[b] && spelbord[a] == spelbord[c])
+            {
+                if (spelbord[a] == "X")
+                {
+                    Status = GameStatus.PlayerXWins;
+                    result = true;
+                }
+                else if (spelbord[a] == "O")
+                {
+                    Status = GameStatus.PlayerOWins;
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+        //Reset
+        public void Reset()
+        {
+            Status = GameStatus.PlayerOPlays;
+            spelbord = new String[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        }
+    }
+}
